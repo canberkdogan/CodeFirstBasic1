c9b230cfabe2898d838e11bd2ebb4de5291bddf3
canbe <canbe@CANBERK>
2024-10-16 23:46:07 +0300

    Add project files.

---
+++ CodeFirstBasic1/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeFirstBasic1.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasic1.Data
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options)
+        {
+            
+        }
+
+        DbSet<GameEntity> Games => Set<GameEntity>();
+        DbSet<MovieEntity> Movies => Set<MovieEntity>();
+    }
+}
---
+++ CodeFirstBasic1/Entities/GameEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CodeFirstBasic1.Entities
+{
+    public class GameEntity
+    {
+        public int Id { get; set; }
+        public  string  Name { get; set; }
+
+        public  string Platform { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+}
